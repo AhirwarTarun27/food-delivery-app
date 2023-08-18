b054cf1592b1e4c12f0a95893beb9965c422fe9b
tarun <ahirwar.tarun@thinksys.com>
2023-08-18 18:56:47 +0530

    added createContext

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  console.log('cartCtx:', cartCtx);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentAmount, item) => {
+    return currentAmount + item.totalAmount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
